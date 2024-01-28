f025195d8a160a12d6f16ec96014ca0632ca0eb7
Ahmed-Bayoumy <ahmed.bayoumy@mail.mcgill.ca>
2024-01-28 17:04:14 -0500

    remove stalml dep

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
       'pandas>=1.5.2',
       'scipy>=1.9.3',
       'setuptools>=58.1.0',
-      'samplersLib>=24.1.3',
-      'StatLML>=2.0.0'
+      'samplersLib>=24.1.3'
     ],
     extras_require={
         'interactive': ['matplotlib>=3.5.2', 'plotly>=5.14.1'],
